e9cdd5f334aa7c65cdc905ddf007815701ab5e33
md-mustaquim-sheikh <mustaquimsheikh786@gmail.com>
2025-05-20 10:43:13 +0530

    modified:   src/components/canvas/Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,25 +10,23 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
+    <Float speed={1.5} rotationIntensity={0.8} floatIntensity={1.5}>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[0, 0, 1]} intensity={0.5} />
+      <mesh castShadow receiveShadow scale={2.5}>
+        <icosahedronGeometry args={[1, 0]} /> {/* Simplified geometry */}
         <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
+          color="#fff8eb"
           flatShading
         />
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
+          rotation={[0, 0, 0]}
+          scale={0.9}
           map={decal}
           flatShading
         />
@@ -39,18 +37,19 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
+    <div className="w-28 h-28"> {/* Ensures size is set for mobile */}
+      <Canvas
+        frameloop="demand"
+        dpr={[1, 1.5]} // Limit DPR for performance
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls enableZoom={false} />
+          <Ball imgUrl={icon} />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
   );
 };
 
